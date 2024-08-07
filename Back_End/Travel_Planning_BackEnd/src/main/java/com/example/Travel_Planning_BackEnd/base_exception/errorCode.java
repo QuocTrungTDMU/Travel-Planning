c7e6923003dc23911af55ae83a04dacb60cb181f
KietLe <letuankiet26102003kl@gmail.com>
2024-08-07 22:07:21 +0700

    congfigion axios and login user back end

--- Back_End/Travel_Planning_BackEnd/src/main/java/com/example/Travel_Planning_BackEnd/base_exception/errorCode.java
+++ Back_End/Travel_Planning_BackEnd/src/main/java/com/example/Travel_Planning_BackEnd/base_exception/errorCode.java
@@ -1,2 +1,20 @@
-package com.example.Travel_Planning_BackEnd.base_exception;public enum errorCode {
+package com.example.Travel_Planning_BackEnd.base_exception;
+
+public enum errorCode {
+    USER_NOT_FOUND(1000,"User Not Found"),
+    EMAIL_USER_ALREADY_EXIST(999,"Email User Already Exist"),
+    ;
+    int idCode;
+    String message;
+    errorCode(int idCode, String message) {
+        this.idCode=idCode;
+        this.message=message;
+    }
+    public int getIdCode() {
+        return idCode;
+    }
+    public String getMessage() {
+        return message;
+    }
+
 }
--- Back_End/Travel_Planning_BackEnd/src/main/java/com/example/Travel_Planning_BackEnd/mapper/userMapper.java
+++ Back_End/Travel_Planning_BackEnd/src/main/java/com/example/Travel_Planning_BackEnd/mapper/userMapper.java
@@ -1,2 +1,11 @@
-package com.example.Travel_Planning_BackEnd.mapper;public interface userMapper {
+package com.example.Travel_Planning_BackEnd.mapper;
+
+import com.example.Travel_Planning_BackEnd.dto.request.userRequest.createUserRequest;
+import com.example.Travel_Planning_BackEnd.entity.Users;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface userMapper {
+    Users toUser(createUserRequest createUserRequest);
+
 }
